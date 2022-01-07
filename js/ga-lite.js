e67e1b5c0d6b8eeee037cdfdba179e5d21479994
STARRY-S <hxstarrys@gmail.com>
2022-01-07 13:26:19 +0800

    remove ga-lite.js

--- js/ga-lite.js
+++
@@ -1,7 +0,0 @@
-(function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)}
-;a=t.createElement(i);c=t.getElementsByTagName(i)[0];a.async=true;a.src=s
-;c.parentNode.insertBefore(a,c)
-})(window,document,"galite","script","https://cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js");
-
-galite('create', 'G-9PH0VRNR7T', 'auto');
-galite('send', 'pageview');
